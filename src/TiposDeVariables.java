694c159569f3f5559415224e0d9f7650af1915fb
isabellanottaro <isabellanottaro@gmail.com>
2025-09-22 11:51:26 +0200

    Ejercicios1-4

--- src/TiposDeVariables.java
+++ src/TiposDeVariables.java
@@ -18,12 +18,30 @@ public class TiposDeVariables {
         // y si tiene armadura (boolean). Imprímelas como si fuese una ficha del jugador.
 
 
+        String nombreJugador = "PacoFiestas" ;
+        long nivel = 31321984;
+        double vida = 20.0;
+        boolean tieneArmadura = true;
+
+        System.out.println("La Ficha de Jugador es: "
+                + "Nombre: " + nombreJugador +
+                " , tiene nivel: " + nivel
+                + " y su vida es " + vida
+                + " y tiene armadura? " + tieneArmadura);
+
+
         // ==============================
         // EJERCICIO 2 – Batalla rápida (Pokémon)
         // ==============================
         // Declara dos variables con el ataque de Pikachu y la defensa de Charmander.
         // Calcula el daño causado (ataque – defensa).
         // Imprime el resultado: "Pikachu hace X puntos de daño a Charmander" - X es el valor real del daño.
+
+        long ataquePikachu = 50;
+        long defensacharmander = 30;
+        long daño = ataquePikachu-defensacharmander;
+        System.out.println(" Pikachu quita a charmander " + daño  + " puntos de vida " );
+
 
 
         // ==============================
@@ -33,6 +51,12 @@ public class TiposDeVariables {
         // Resta el precio de una skin (ej: 1200).
         // Imprime el resultado: "Después de comprar la skin, te quedan X V-Bucks".
 
+        String nombrejugador = "Cazorla" ;
+        long Pavos= 3200;
+        long skin= 1200;
+        long PavosRestantes= (Pavos-skin);
+        String pavos = " VBucks " ;
+        System.out.println( "Después de comprar a Frozono, te quedan: " + PavosRestantes + pavos + nombrejugador );
 
         // ==============================
         // EJERCICIO 4 – Velocidad de campeón (League of Legends)
@@ -41,6 +65,11 @@ public class TiposDeVariables {
         // y otra con un buff de velocidad.
         // Calcula la velocidad final sumando ambos valores.
         // Imprime el resultado: "La velocidad final de Jinx es X".
+
+        long VelocidadYone= 345;
+        long BotasVelocidad=150;
+        long VelocidadNuevaYone= (VelocidadYone+BotasVelocidad);
+        System.out.println(" La velocidad final de Yone es " + VelocidadNuevaYone );
 
 
         // ==============================
